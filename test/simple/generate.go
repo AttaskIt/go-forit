137a937f9db3e15fe1d5519e7da05f71417c389e
long.vo <hoanglongvo2006@gmail.com>
2025-02-23 12:39:09 +0100

    fix: generate annotation for simple example

--- test/simple/generate.go
+++ test/simple/generate.go
@@ -1,3 +1,3 @@
-package simple 
+package simple
 
-//go:generate protoc --go-go-forit_out=. --go-go-forit_opt=paths=source_relative --go_out=. --go_opt=paths=source_relative -I . simple.proto
+//go:generate protoc --go-forit_out=. --go-forit_opt=paths=source_relative --go_out=. --go_opt=paths=source_relative -I . simple.proto
